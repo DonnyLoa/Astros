4f578a281a919376fbadc87bc1772cb24d0418cc
Carter Bryant <donny.lao64@gmail.com>
2018-07-28 15:29:07 -0700

    Added
    TitleScreen class added
    Name class added

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import webapp2
+import jinja2
+import os
 
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class TitleScreen(webapp2.RequestHandler):
+    def get(self):
+        start_template = JINJA_ENVIRONMENT.get_template('(title_screen.html)')
+        self.response.write(start_template.render())
+
+class Name(webapp2.RequestHandler):
+    def get(self):
+        start_template = JINJA_ENVIRONMENT.get_template('')
+        self.response.write(start_template.render())
+
+app = webapp2.WSGIApplication([
+    ('/mainPage', TitleScreen),
+    ('/name', Name),
+], debug=True)
