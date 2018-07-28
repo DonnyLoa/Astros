910e65b6f8f69adf23b39fc40cefc5ede5629cc8
Dee Harris <darlingirlh@gmail.com>
2018-07-28 15:38:50 -0700

    difficulty classes
    further split up categories according to difficulty

--- model.py
+++ model.py
@@ -1,11 +1,32 @@
 from google.appengine.ext import ndb
 
-class Animals(ndb.Model):
-    animalQuestion = ndb.StringProperty(required=True)
-    animalCorrect = ndb.StringProperty(required=True)
-    animalWrong = ndb.StringProperty(repeated=True)
+class easyAnimals(ndb.Model):
+    animal_e_question = ndb.StringProperty(required=True)
+    animal_e_correct = ndb.StringProperty(required=True)
+    animal_e_wrong = ndb.StringProperty(repeated=True)
 
-class Geography(ndb.Model):
-    geoQuestion = ndb.StringProperty(required=True)
-    geoCorrect = ndb.StringProperty(required=True)
-    geoWrong = ndb.StringProperty(repeated=True)
+class mediumAnimals(ndb.Model):
+    animal_m_question = ndb.StringProperty(required=True)
+    animal_m_correct = ndb.StringProperty(required=True)
+    animal_m_wrong = ndb.StringProperty(repeated=True)
+
+class hardAnimals(ndb.Model):
+    animal_h_question = ndb.StringProperty(required=True)
+    animal_h_correct = ndb.StringProperty(required=True)
+    animal_h_wrong = ndb.StringProperty(repeated=True)
+
+
+class easyGeography(ndb.Model):
+    geo_e_question = ndb.StringProperty(required=True)
+    geo_e_correct = ndb.StringProperty(required=True)
+    geo_e_wrong = ndb.StringProperty(repeated=True)
+
+class mediumGeography(ndb.Model):
+    geo_m_question = ndb.StringProperty(required=True)
+    geo_m_correct = ndb.StringProperty(required=True)
+    geo_m_wrong = ndb.StringProperty(repeated=True)
+
+class hardGeography(ndb.Model):
+    geo_h_question = ndb.StringProperty(required=True)
+    geo_h_correct = ndb.StringProperty(required=True)
+    geo_h_wrong = ndb.StringProperty(repeated=True)
