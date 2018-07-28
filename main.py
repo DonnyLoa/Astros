8eef06c067523a062e844ac213d4c9641397f6ba
Dee Harris <darlingirlh@gmail.com>
2018-07-28 15:51:37 -0700

    added beginnings of seed page

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import webapp2
 import jinja2
 import os
+import json
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
+
+class SeedPage(webapp2.RequestHandler):
+    def get(self):
+        animalsEasyEndPoint = "https://opentdb.com/api.php?amount=10&category=27&difficulty=easy"
+        animalsMediumEndPoint = "https://opentdb.com/api.php?amount=10&category=27&difficulty=medium"
+        animalsHardEndPoint = "https://opentdb.com/api.php?amount=10&category=27&difficulty=hard"
+        geoEasyEndpoint = "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy"
+        geoMediumEndPoint = "https://opentdb.com/api.php?amount=10&category=22&difficulty=medium"
+        geoHardEndPoint = "https://opentdb.com/api.php?amount=10&category=22&difficulty=hard"
 
 class TitleScreen(webapp2.RequestHandler):
     def get(self):
@@ -20,4 +30,5 @@ class Name(webapp2.RequestHandler):
 app = webapp2.WSGIApplication([
     ('/mainPage', TitleScreen),
     ('/name', Name),
+    ('/seed-page', SeedPage)
 ], debug=True)
